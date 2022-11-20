9c31fe1354d2f5d94607fd5f86b3e43e3085e828
Chanwanit-Nuchyoo <peaknuchyoo@gmail.com>
2022-11-21 04:07:15 +0700

    -add calendarDashbroad (need an api to fetch data)

--- src/constants/text.js
+++ src/constants/text.js
@@ -7,6 +7,8 @@ import {
   guestsUrl,
 } from "./pathUrl.js";
 
+import moment from "moment";
+
 export const menu = [
   { title: "Dashboard", path: `${dashboardUrl}` },
   { title: "Calendar", path: `${calendarUrl}` },
@@ -268,3 +270,67 @@ export const table2 = {
         }
     ],
 }
+
+export const Calendar_DB_Data = [
+  {
+    roomType: "Superior Room",
+    data: [6,7,0,9,15,11,9,8,12,2,2,7,0,9]
+  },
+  {
+    roomType: "Deluxe Room",
+    data: [5,2,5,2,2,3,9,2,1,3,6,4,2,5]
+  },
+  {
+    roomType: "Beach House",
+    data: [2,1,3,0,0,1,0,0,0,1,1,1,0,2]
+  },
+  {
+    roomType: "Family Premium",
+    data: [2,1,0,0,0,3,1,2,1,0,1,0,0,2]
+  }
+]
+
+const today = moment();
+const dateList = [...Array(14)].map((d, index) => moment(today).add(index, "days"))
+const dayOrdinalSF = (date) => {
+const _date = parseInt(date);
+const lastNumber = _date % 10;
+  switch (lastNumber) {
+    case 1:
+      return date + " st";
+    case 2:
+      return date + " nd";
+    case 3:
+      return date + " rd";
+    default:
+      return date + " th";
+  }
+};
+
+const labels = dateList.map((e,index)=> e.format('ddd') + " " + dayOrdinalSF(e.format("D")))
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: '',
+    },
+  },
+};
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Booked Rooms',
+      data: [50,30,40,55,15,20,25,25,40,50,50,30,25,20],
+      borderColor: 'rgb(212, 153, 13)',
+      backgroundColor: 'rgba(212, 153, 13, 0.5)',
+    },
+  ],
+};
+ 
